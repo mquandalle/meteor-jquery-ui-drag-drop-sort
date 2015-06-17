b9debe35c05a2812336dd166176927f9f5c7005d
Maxime Quandalle <maxime@quandalle.com>
2015-06-17 12:44:19 +0200

    First commit

---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  name: 'mquandalle:jquery-ui-drag-drop-sort',
+  version: '0.1.0',
+  // Brief, one-line summary of the package.
+  summary: 'A wrapper for jquery-ui a custom build',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/mquandalle/meteor-jquery-ui-drag-drop-sort',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: null
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles(['jquery-ui.js', 'jquery-ui.css'], 'client');
+});
